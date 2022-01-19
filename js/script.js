6430c6f9a3bf6ff7d303d42506799620781ac26b
Hans M. Boron <hansmateusboron@gmail.com>
2022-01-19 11:32:28 -0300

    make validation stronger

--- js/script.js
+++ js/script.js
@@ -17,28 +17,39 @@ form.addEventListener('submit', e => {
     var last_name_error = null;
     var email_error = null;
     var password_error = null;
+    var first_name_value = first_name.value.trim();
+    var last_name_value = last_name.value.trim();
+    var email_value = email.value.trim();
+    var password_value = password.value.trim();
     resetErrorMessages();
     first_name.style.borderColor = '#c7c7c7';
     last_name.style.borderColor = '#c7c7c7';
     email.style.borderColor = '#c7c7c7';
     password.style.borderColor = '#c7c7c7';
-    if (first_name.value === '' || first_name.value == null) {
+
+    if (first_name_value === '' || first_name_value == null) {
         first_name_error = 'First Name cannot be empty';
-        first_name.style.borderColor = 'hsl(0, 100%, 74%)'
+        first_name.style.borderColor = 'hsl(0, 100%, 74%)';
     }
 
-    if (last_name.value === '' || last_name.value == null) {
+    if (last_name_value === '' || last_name_value == null) {
         last_name_error = 'Last Name cannot be empty';
-        last_name.style.borderColor = 'hsl(0, 100%, 74%)'
+        last_name.style.borderColor = 'hsl(0, 100%, 74%)';
     }
 
-    if (email.value === '' || email.value == null) {
+    if (email_value === '' || email_value == null) {
         email_error = 'Email cannot be empty';
+        email.style.borderColor = 'hsl(0, 100%, 74%)';
+    } else if (!isValidEmail(email_value)) {
+        email_error = 'Looks like this is not an email';
         email.style.borderColor = 'hsl(0, 100%, 74%)';
     }
 
-    if (password.value === '' || password.value == null) {
+    if (password_value === '' || password_value == null) {
         password_error = 'Password cannot be empty';
+        password.style.borderColor = 'hsl(0, 100%, 74%)';
+    } else if (password_value.length < 8) {
+        password_error = 'Password must have at least 8 characters';
         password.style.borderColor = 'hsl(0, 100%, 74%)';
     }
 
@@ -61,4 +72,9 @@ function resetErrorMessages() {
     last_name_message.innerText = null;
     email_message.innerText = null;
     password_message.innerText = null;
+}
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
